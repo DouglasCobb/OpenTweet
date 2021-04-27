ff7f9a2f89a5e1073da796778a78a54ddc5c86e3
Douglas Cobb <douglas.g.cobb@gmail.com>
2021-04-27 15:50:24 -0600

    Added mention and tweet highlighting. Also added loading of avatar images

--- OpenTweet/Cells/TweetTableViewCell.swift
+++ OpenTweet/Cells/TweetTableViewCell.swift
@@ -21,9 +21,50 @@ class TweetTableViewCell: UITableViewCell {
     
     // MARK: Private
     
+    @IBOutlet weak private var avatarImageView: UIImageView!
     @IBOutlet weak private var authorLabel: UILabel!
     @IBOutlet weak private var dateLabel: UILabel!
     @IBOutlet weak private var contentLabel: UILabel!
+    
+    private var convertedContent: NSAttributedString? {
+        
+        // Guard Content
+        guard let content = tweet?.content else { return nil }
+        
+        // Process Content
+        do {
+            
+            // Converted Content
+            let convertedContent = NSMutableAttributedString(string: content)
+            
+            // Mentions
+            let mentionRegex = try NSRegularExpression(pattern: "@\\S*", options: [])
+            let mentions = mentionRegex.matches(in: content, options: [], range: NSMakeRange(0, content.count))
+            
+            // Format Mentions
+            for mention in mentions {
+                
+                // Add Color
+                convertedContent.addAttribute(.foregroundColor, value: tintColor ?? .blue, range: mention.range)
+            }
+            
+            // Link Regex
+            let linkRegex = try NSRegularExpression(pattern: "(www|http:|https:)+[^\\s]+[\\w]", options: [])
+            let links = linkRegex.matches(in: content, options: [], range: NSMakeRange(0, content.count))
+            
+            // Format Links
+            for link in links {
+                
+                // Add Color
+                convertedContent.addAttribute(.foregroundColor, value: tintColor ?? .blue, range: link.range)
+            }
+            
+            return convertedContent
+            
+        } catch {
+            return nil
+        }
+    }
 
     override func awakeFromNib() {
         
@@ -46,9 +87,26 @@ extension TweetTableViewCell {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         
-        // Update Text
+        // Set Placeholder Image
+        avatarImageView.image = UIImage(named: "employeePlaceholder")
+        
+        // Avatar
+        avatarImageView.isHidden = tweet?.avatar == nil
+        if let avatar = tweet?.avatar, let avatarURL = NSURL(string: avatar) {
+            
+            // Load Image
+            ImageCache.publicCache.load(avatarURL) { [weak self] image in
+                
+                // Verify Current URL
+                if avatar == self?.tweet?.avatar {
+                    self?.avatarImageView.image = image
+                }
+            }
+        }
+        
+        // Author / Date Text
         authorLabel.text = tweet?.author
         dateLabel.text = (tweet == nil) ? "N/A" : dateFormatter.string(from: tweet!.date)
-        contentLabel.text = tweet?.content
+        contentLabel.attributedText = convertedContent
     }
 }
---
+++ OpenTweet/Helper/ImageCache.swift
@@ -0,0 +1,68 @@
+import UIKit
+import Foundation
+
+
+//
+//  ImageCache.swift
+//  OpenTweet
+//
+//  Created by Douglas Cobb on 4/26/21.
+//  Copyright © 2021 OpenTable, Inc. All rights reserved.
+//
+
+
+public class ImageCache {
+    
+    public static let publicCache = ImageCache()
+    var placeholderImage = UIImage(systemName: "rectangle")!
+    private let cachedImages = NSCache<NSURL, UIImage>()
+    private var loadingResponses = [NSURL: [(UIImage?) -> Void]]()
+    
+    public final func image(url: NSURL) -> UIImage? {
+        return cachedImages.object(forKey: url)
+    }
+    
+    final func load(_ url: NSURL, completion: @escaping (UIImage?) -> Void) {
+        
+        // Check for a cached image.
+        if let cachedImage = image(url: url) {
+            DispatchQueue.main.async {
+                completion(cachedImage)
+            }
+            return
+        }
+        
+        // In case there are more than one requestor for the image, we append their completion block.
+        if loadingResponses[url] != nil {
+            loadingResponses[url]?.append(completion)
+            return
+        } else {
+            loadingResponses[url] = [completion]
+        }
+        
+        // Go fetch the image.
+        URLSession.shared.dataTask(with: url as URL) { (data, response, error) in
+            
+            // Check for the error, then data and try to create the image.
+            guard let responseData = data, let image = UIImage(data: responseData),
+                let blocks = self.loadingResponses[url], error == nil else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            
+            // Cache the image.
+            self.cachedImages.setObject(image, forKey: url, cost: responseData.count)
+            
+            // Iterate over each requestor for the image and pass it back.
+            for block in blocks {
+                DispatchQueue.main.async {
+                    block(image)
+                }
+                return
+            }
+            
+        }.resume()
+    }
+}
